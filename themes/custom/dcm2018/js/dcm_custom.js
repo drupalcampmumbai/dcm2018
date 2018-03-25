40dad4dbd3b9b11b94f115b998ab9ecbb70d7945
Ashish Vitthal Dalvi <ashish.vitthal.dalvi@gmail.com>
2018-03-25 13:14:43 +0530

    SYnc the folder structure from pantheon

--- themes/custom/dcm2018/js/dcm_custom.js
+++ themes/custom/dcm2018/js/dcm_custom.js
@@ -14,18 +14,6 @@ jQuery(document).ready(function($){
     /*End Navigation menu on hover*/
 
     /*Propose session get heighest session height and apply to all session block*/
-    var highest = null;
-    var hi = 0;
-    $(".view-sessions .owl-item ul li").each(function(){
-        var h = $(this).height();
-        if(h > hi){
-         hi = h;
-        }    
-    });
-    $(".view-sessions .owl-item ul li").height(hi);
-    /*End Propose session get heighest session height and apply to all session block*/
-
-    /*Propose session get heighest session height and apply to all session block*/
     var h1;
     $(".view-sponsors .views-field-body").each(function(){
         h1 = $(this).outerHeight(true);
