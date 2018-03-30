8835cd4ba58beff0c8e4a962d0562cbcad1c8ac1
Kiran Kadam <kirankadam@Kirans-MacBook-Air.local>
2018-03-30 20:50:26 +0530

    homepage revamp

--- themes/custom/dcm2018/js/dcm_custom.js
+++ themes/custom/dcm2018/js/dcm_custom.js
@@ -1,17 +1,21 @@
 jQuery(document).ready(function($){
 	/*Navigation menu on hover*/
-    $(".dropdown").hover(            
-    function() {
-        $('.dropdown-menu', this).stop( true, true ).fadeIn("fast");
-        $(this).toggleClass('open');
-        $('span', this).toggleClass("caret caret-up");                
-    },
-    function() {
-        $('.dropdown-menu', this).stop( true, true ).fadeOut("fast");
-        $(this).toggleClass('open');
-        $('span', this).toggleClass("caret caret-up");                
-    });
+    // $(".dropdown").hover(            
+    // function() {
+    //     $('.dropdown-menu', this).stop( true, true ).fadeIn("fast");
+    //     $(this).toggleClass('open');
+    //     $('span', this).toggleClass("caret caret-up");                
+    // },
+    // function() {
+    //     $('.dropdown-menu', this).stop( true, true ).fadeOut("fast");
+    //     $(this).toggleClass('open');
+    //     $('span', this).toggleClass("caret caret-up");                
+    // });
     /*End Navigation menu on hover*/
+
+    $('#navbar .navbar-toggle').on('click', function (e) {
+        $('.navbar-header').toggleClass('menu-open');
+    });
 
     /*Propose session get heighest session height and apply to all session block*/
     var h1;
